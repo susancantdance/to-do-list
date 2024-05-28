8c65c577c4ec0d67a59d8ccbf8801d1351a4d1c9
susancantdance <susan.l.chen@gmail.com>
2024-05-28 13:31:22 -0700

    adding packages

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,30 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  mode: 'production',
-  entry: './src/index.js',
-  devtool: 'inline-source-map',
+  mode: "production",
+  entry: "./src/index.js",
+  devtool: "inline-source-map",
   plugins: [
     new HtmlWebpackPlugin({
-     title: 'Development',
-     template: '/src/index.html'
-    })
+      title: "Development",
+      template: "/src/index.html",
+    }),
   ],
   module: {
-      rules: [
+    rules: [
       {
         test: /\.jpg/,
-        type: 'asset/resource'
+        type: "asset/resource",
       },
       {
         test: /\.css$/i,
         use: ["style-loader", "css-loader"],
       },
-    ]
+    ],
   },
   output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist')
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
   },
 };
